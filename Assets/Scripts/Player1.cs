9308a6057bb479ae59f9328d6c42a78d86e14d71
NathanTDo <nathantdo04@gmail.com>
2020-07-22 21:26:21 -0700

    Added inputs and laser fire

--- Assets/Scripts/Player1.cs
+++ Assets/Scripts/Player1.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player1 : MonoBehaviour
 {
     // config params
     [SerializeField] float moveSpeed = 10f;
